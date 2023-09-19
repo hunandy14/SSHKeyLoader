40b468de0485f0ca7df0d85a40a28111dc6793d9
Charlotte.Hong <hunandy14@gmail.com>
2023-09-20 02:26:45 +0900

    初版完成

---
+++ SSHKeyLoader.ps1
@@ -0,0 +1,47 @@
+# 上傳公鑰
+function Add-SSHKeyToServer {
+    [CmdletBinding(DefaultParameterSetName = "Path")]
+    param (
+        [Parameter(Position=0, ParameterSetName="", Mandatory)]
+        [string] $User,
+        [Parameter(Position=1, ParameterSetName="", Mandatory)]
+        [string] $HostName,
+        [Parameter(Position=2, ParameterSetName="Path")]
+        [string] $Path,
+        [Parameter(ParameterSetName="PubKeyContent")]
+        [string] $PubKeyContent
+    )
+    
+    # 新增金鑰
+    if (!$Path -and !$PubKeyContent) {
+        $Path = "$env:USERPROFILE\.ssh\id_ed25519.pub"
+        if (!(Test-Path $Path -PathType Leaf)) { ssh-keygen -t ed25519 -f $Path }
+    }
+    
+    # 處理路徑
+    if ($Path) {
+        [IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath))
+        $Path = [IO.Path]::GetFullPath($Path)
+        $PubKeyContent = Get-Content $Path
+    }
+    
+    # 上傳公鑰
+    if ($PubKeyContent) {
+        # 檢索用字串
+        $SearchContent = ($PubKeyContent -split " ")[1]
+        if ($SearchContent.Length -ge 254) { $SearchContent = (($SearchContent)).Substring($SearchContent.Length - 254) }
+        $SearchContent = [regex]::Escape($SearchContent)
+        
+        # 上傳公鑰
+        ssh $User@$HostName "whoami /groups | findstr /C:S-1-5-32-544 >nul && ((findstr """$SearchContent""" C:\ProgramData\ssh\administrators_authorized_keys >nul || (echo $PubKeyContent>>C:\ProgramData\ssh\administrators_authorized_keys)) && (icacls.exe C:\ProgramData\ssh\administrators_authorized_keys /inheritance:r /grant Administrators:F /grant SYSTEM:F >nul)) || ((if not exist .ssh mkdir .ssh) && (findstr """/C:$SearchContent""" .ssh\authorized_keys >nul || (echo $PubKeyContent>>.ssh\authorized_keys)))"
+        Write-Host "$PubKeyContent" -ForegroundColor DarkGray
+        
+        # 成功信息
+        ssh -o BatchMode=yes $User@$HostName "echo Upload successful. Now connected to $User@$HostName"
+    }
+}
+# Add-SSHKeyToServer sftp 192.168.3.123 $env:USERPROFILE\.ssh\id_ed25519.pub
+# Add-SSHKeyToServer administrator 192.168.3.123 $env:USERPROFILE\.ssh\id_ed25519.pub
+# Add-SSHKeyToServer administrator 192.168.3.123 $env:USERPROFILE\.ssh\id_rsa.pub
+# Add-SSHKeyToServer sftp 192.168.3.123 -PubKeyContent (Get-Content $env:USERPROFILE\.ssh\id_ed25519.pub)
+# Add-SSHKeyToServer sftp 192.168.3.123
